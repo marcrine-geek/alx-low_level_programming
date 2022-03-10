310d2e272e3e7f4c2f32bdbe456431b69e56e22d
marcrine-geek <marcrinemm@gmail.com>
2022-03-10 01:42:13 -0800

    C program to print an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*Printing out a string*/
-int main(void){
+/**
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success.
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
