c982bf5d997511c50d6ceb6ea9c710eb63e33303
marcrine-geek <marcrinemm@gmail.com>
2022-03-11 04:06:39 -0800

    printing combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+
+		if (x < 9)
+		{
+			putchar(x + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else if (x == 9)
+		{
+			putchar(x + '0');
+		}
+	}
+	return (0);
+}
+
