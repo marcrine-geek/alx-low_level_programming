4d98f4b2a522e1dc73cf798549c97309329576cc
marcrine-geek <marcrinemm@gmail.com>
2022-03-11 03:17:40 -0800

    printing integers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+
+	}
+	return (0);
+}
