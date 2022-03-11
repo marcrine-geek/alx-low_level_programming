7febac7e0b3cc4e9108feb79658e181a8343cdff
marcrine-geek <marcrinemm@gmail.com>
2022-03-11 03:23:53 -0800

    printing alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+	for (x = 'Z'; x >= 'A'; x--)
+	{
+		putchar(tolower(x));
+	}
+	return (0);
+}
