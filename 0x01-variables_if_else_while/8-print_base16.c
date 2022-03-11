8dcd353b5f7bf5e1aa14c31bb406c29834334f60
marcrine-geek <marcrinemm@gmail.com>
2022-03-11 03:38:06 -0800

    printing base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int x;
+	int y;
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	for (y = 'a'; y < 'g'; y++)
+	{
+		putchar(tolower(y));
+	}
+	return (0);
+}
