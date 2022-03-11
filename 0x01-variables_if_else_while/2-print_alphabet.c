0101a6bae5fbf6c45fc64425903367089a1abb45
marcrine-geek <46963546+marcrine-geek@users.noreply.github.com>
2022-03-11 16:50:30 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 /**
  * main - Entry point
  *
@@ -9,9 +8,9 @@ int main(void)
 {
 	int x;
 
-	for (x = 'A'; x <= 'Z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
-		putchar(tolower(x));
+		putchar(x);
 	}
 	return (0);
 }
