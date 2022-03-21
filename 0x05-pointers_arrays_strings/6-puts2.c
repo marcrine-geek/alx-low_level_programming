074fe2987095b713106696a039359692cead5b98
marcrine-geek <marcrinemm@gmail.com>
2022-03-21 05:15:07 -0700

    printing characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function to print characters of a string
+ *
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	while (*str != 0)
+	{
+		if (*str % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str++;
+	}
+	_putchar('\n');
+}
